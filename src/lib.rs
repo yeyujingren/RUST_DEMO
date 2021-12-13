fb591c26936b40a25d30058df8ba4d8d061d4063
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-12-13 12:12:52 +0800

    chapter 15

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,112 @@
-// //! # minigrep
-// //! 
-// //! `minigrep` is a collection of utilities to make performing certain
-// //! calculations more convenient
+pub trait Messenger {
+  fn send(&self, msg: &str);
+}
 
-// /// Adds one to the number given.
-// /// 
-// /// # Example
-// /// 
-// /// ```
-// /// let arg = 5;
-// /// let answer = minigrep::add_one(arg);
-// /// 
-// /// assert_eq!(6, answer)
-// /// ```
-// pub fn add_one(x: i32) -> i32 {
-//   x+1
+pub struct LimitTracker<'a, T: Messenger> {
+  message: &'a T,
+  value: usize,
+  max: usize
+}
+
+impl<'a, T> LimitTracker<'a, T> 
+  where T: Messenger {
+    pub fn new(message: & T, max: usize) -> LimitTracker<T> {
+      LimitTracker {
+        message,
+        value: 0,
+        max
+      }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+      self.value = value;
+      let percentage_of_max = self.value as f64 / self.max as f64;
+      if percentage_of_max >= 1.0 {
+        self.message.send("Error: you are over your quota!");
+      } else if percentage_of_max >= 0.9 {
+        self.message.send("Urgent warning: You've used up over 90% your quota!");
+      } else if percentage_of_max >= 0.75 {
+        self.message.send("Warning: You've used up over 75% your quota!");
+      }
+    }
+  }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::cell::RefCell;
+
+  struct MockMessage {
+    sent_messages: RefCell<Vec<String>>,
+  }
+
+  impl MockMessage {
+    fn new () -> MockMessage {
+      MockMessage {
+        sent_messages: RefCell::new(vec![])
+      }
+    }
+  }
+
+  impl Messenger for MockMessage {
+    fn send(&self, message: &str) {
+      // self.send_message.push(String::from(message));
+      self.sent_messages.borrow_mut().push(String::from(message));
+    }
+  }
+
+  #[test]
+  fn in_send_an_over_75_percent_warning_message() {
+    let mock_message = MockMessage::new();
+
+    let mut limit_tracker = LimitTracker::new(&mock_message, 100);
+    limit_tracker.set_value(80);
+
+    assert_eq!(mock_message.sent_messages.borrow().len(), 1);
+  }
+
+  // Err demo
+  // impl Messenger for MockMessage {
+  //   fn send(&self, message: &str) {
+  //     let mut one_borrow = self.sent_messages.borrow_mut();
+  //     let mut two_borrow = self.sent_messages.borrow_mut();
+
+  //     one_borrow.push(String::from(message));
+  //     two_borrow.push(String::from(message));
+  //   }
+  // }
+}
+
+// ERR TEST
+// #[cfg(test)]
+// mod tests {
+//   use super::*;
+//   struct MockMessage {
+//     send_message: Vec<String>
+//   }
+
+//   impl MockMessage {
+//     fn new() -> MockMessage {
+//       MockMessage {
+//         send_message: vec![]
+//       }
+//     }
+//   }
+
+//   impl Messenger for MockMessage {
+//     fn send(&self, message: &str) {
+//       self.send_message.push(String::from(message));
+//     }
+//   }
+
+//   #[test]
+//   fn test_demo() {
+//     let mock_messager = MockMessage::new();
+//     let mut limit_tracker = LimitTracker::new(&mock_messager, 100);
+    
+//     limit_tracker.set_value(80);
+
+//     assert_eq!(mock_messager.send_message.len(), 1);
+
+//   }
 // }
-
-//! # Art
-//! 
-//! A library modeling artistic concept
-
-pub use self::kinds::PrimaryColor;
-pub use self::kinds::SecondaryColor;
-pub use self::utils::mix;
-
-pub mod kinds {
-  /// The primary colors according to the RYB color model
-  pub enum PrimaryColor {
-    Red,
-    Yellow,
-    Blue
-  }
-
-  // Then secondary colors according to the RYB color model
-  pub enum SecondaryColor {
-    Orange,
-    Green,
-    Purple,
-  }
-}
-
-pub mod utils {
-  use crate::kinds::*;
-
-  /// Combines two primary colrs in equal amounts to create
-  /// a secondary color
-  pub fn mix(c1: PrimaryColor, c2: SecondaryColor) -> SecondaryColor {
-    c2
-  }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,185 @@
-use minigrep::PrimaryColor;
-use minigrep::SecondaryColor;
-use minigrep::mix;
+// use crate::List::{ Cons, Nil };
+// use std::ops::Deref;
+// use std::mem::drop;
+// use std::rc::Rc;
+
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil
+// }
+
+// struct MyBox<T>(T);
+// impl<T> MyBox<T> {
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+//     fn deref(&self) -> &T {
+//         &self.0
+//     }
+// }
+
+// struct CustomSmartPointer {
+//     data: String
+// }
+
+// impl Drop for CustomSmartPointer {
+//     fn drop(&mut self) {
+//         println!("Dropping CustomSmartPointer with data: {}", self.data);
+//     }
+// }
+
+// fn main() {
+    // let list = Cons(2, Cons(3, Nil));
+    // let list = Cons(1,
+    //     Box::new(
+    //         Cons(2,
+    //             Box::new(
+    //                 Cons(3,
+    //                     Box::new(Nil)
+    //                 )
+    //             )
+    //         )
+    //     )
+    // );
+
+    // let x = 5;
+    // let y = MyBox::new(x);
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    // let m = MyBox::new(String::from("Rust"));
+    // let string = String::from("123");
+    // hello(&(*string)[..]);
+
+    // let c = CustomSmartPointer {
+    //     data: String::from("my stiff")
+    // };
+
+    // let d = CustomSmartPointer {
+    //     data: String::from("other stuff")
+    // };
+    // println!("CustomSmartPointer created.");
+    // drop(c);
+    // println!("CustomSmartPointer dropped before the end of main");
+
+    // let a = Rc::new(Cons(5,
+    //     Rc::new(
+    //         Cons(10,
+    //             Rc::new(Nil)
+    //         )
+    //     )
+    // ));
+    // println!("count after creating a = {}", Rc::strong_count(&a));
+    // let b = Cons(3, a.clone());
+    // println!("count after creating b = {}", Rc::strong_count(&a));
+    // {
+    //     let c = Cons(4, Rc::clone(&a));
+    //     println!("count after creating c = {}", Rc::strong_count(&a));
+    // }
+    // println!("count after goes out of scope = {}", Rc::strong_count(&a));
+// }
+
+// fn hello(name: &str) {
+//     println!("Hello, {}!", name);
+// }
+
+
+// 15.6-1 循环引用
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// use crate::List::{Cons, Nil};
+
+// #[derive(Debug)]
+// enum List {
+//     Cons(i32, RefCell<Rc<List>>),
+//     Nil,
+// }
+
+// impl List {
+//     fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+//         match self {
+//             Cons(_, item) => Some(item),
+//             Nil => None
+//         }
+//     }
+// }
+
+
+
+// fn main() {
+//     let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+//     println!("a initial rc count = {}", Rc::strong_count(&a));
+//     println!("a next item = {:?}", a.tail());
+
+//     let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+//     println!("a rc count after b creation = {}", Rc::strong_count(&a));
+//     println!("b initial rc count = {}", Rc::strong_count(&b));
+//     println!("b next item = {:?}", b.tail());
+
+//     if let Some(link) = a.tail() {
+//         *link.borrow_mut() = Rc::clone(&b);
+//     }
+
+//     println!("b rc count after changing a = {}", Rc::strong_count(&b));
+//     println!("a rc count after change a = {}", Rc::strong_count(&a));
+// }
+
+// 15.6-2 Weak引用
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>
+}
 
 fn main() {
-    let red = PrimaryColor::Red;
-    let orange = SecondaryColor::Orange;
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new())
+    });
 
-    mix(red, orange);
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)])
+        });
+    
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
