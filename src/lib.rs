12c139664a9ed7ed82568ca01a83b752ab8288a3
yifeng.tao <yifeng.tao@luckincoffee.com>
2021-12-23 09:49:37 +0800

    chapter17.2 commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,76 @@
+// 17.1
+// pub struct AveragedCollection {
+//   list: Vec<i32>,
+//   average: f64
+// }
+
+// impl AveragedCollection {
+//   pub fn add(&mut self, value: i32) {
+//     self.list.push(value);
+//     self.update_average();
+//   }
+
+//   pub fn remove(&mut self) -> Option<i32> {
+//     let result = self.list.pop();
+//     match result {
+//       Some(val) => {
+//         self.update_average();
+//         Some(val)
+//       },
+//       None => None
+//     }
+//   }
+
+//   pub fn average(&self) -> f64 {
+//     self.average
+//   }
+
+//   fn update_average(&mut self) {
+//     let total: i32 = self.list.iter().sum();
+//     self.average = total as f64 / self.list.len() as f64;
+//   }
+// }
+
+
+// 17.2
+// pub trait Draw {
+//   fn draw(&self);
+// }
+
+// pub struct Screen {
+//   pub components: Vec<Box<dyn Draw>>
+// }
+
+// impl Screen {
+//   pub fn run(&self) {
+//     for component in self.components.iter() {
+//       component.draw();
+//     }
+//   }
+// }
+
+// // pub struct Screen<T: Draw> {
+// //   pub components: Vec<T>
+// // }
+
+// // impl<T> Screen<T>
+// //   where T: Draw {
+// //     pub fn run (&self) {
+// //       for component in self.components.iter() {
+// //         component.draw()
+// //       }
+// //     }
+// //   }
+
+// pub struct Button {
+//   pub width: u32,
+//   pub height: u32,
+//   pub label: String
+// }
+
+// impl Draw for Button {
+//   fn draw(&self) {
+
+//   }
+// }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    
-}
+// use minigrep::{Draw, Screen, Button};
+
+// struct SelectBox {
+//     width: u32,
+//     height: u32,
+//     options: Vec<String>,
+// }
+
+// impl Draw for SelectBox {
+//     fn draw(&self) {
+//         // do something
+//     }
+// }
+
+// fn main() {
+//     let screen = Screen {
+//         components: vec![
+//             Box::new(SelectBox {
+//                 width: 75,
+//                 height: 10,
+//                 options: vec![
+//                     String::from("Yes"),
+//                     String::from("maybe"),
+//                     String::from("No")
+//                 ]
+//             }),
+//             Box::new(Button {
+//                 width: 50,
+//                 height: 10,
+//                 label: String::from("Ok")
+//             })
+//         ]
+//     };
+
+//     screen.run();
+
+//     // let err_screen_demo = Screen {
+//     //     components: vec![
+//     //         Box::new(String::from("Hi"))
+//     //     ]
+//     // };
+// }
